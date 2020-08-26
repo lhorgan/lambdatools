13c3273e8987760deee1cac06c43881c949a960c
Luke Horgan <horgan.l@husky.neu.edu>
2020-08-25 21:48:12 -0400

    EC2 creation script, and a shell script to configure the EC2 instance once launched

---
+++ ec2_dist_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install node
+npm install pm2@latest -g
+git clone https://github.com/lhorgan/distributor.git distributor
+pm2 start distributor/distributor.js
