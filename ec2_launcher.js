ef327a0192d8abde1144d64143e3831f22dde6bc
Luke Horgan <horgan.l@husky.neu.edu>
2020-08-26 02:46:23 -0400

    launching ec2 instnaces works

--- ec2_launcher.js
+++ ec2_launcher.js
@@ -1,21 +1,55 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+const fs = require("fs");
+// Load credentials and set region from JSON file
+
+// Create EC2 service object
+var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
 class EC2Launcher {
   constructor() {
+    // https://wiki.debian.org/Cloud/AmazonEC2Image/Buster
+    // US-based 
     this.amis = {
-      "us_east_1": "ami-05c0d7f3fffb419c8",
+      "us-east-1": "ami-05c0d7f3fffb419c8",
       "us-east-2": "ami-03c3603751b46f895",
       "us-west-1": "ami-04a823dd6e4f6fd52",
       "us-west-2": "ami-0f7939d313699273c"
     }
   }
 
-  base64_encode(file_path) {
-    // read binary data
-    var file = fs.readFileSync(file_path);
-    // convert binary data to base64 encoded string
-    return new Buffer(file).toString('base64');
+  go() { // for testing
+    this.createInstance("t2.micro", "testingFromNode", "us-east-1", "h6", 1, "./ec2_dist_config.sh");
+    //this.getKeys("us-east-1");
   }
 
+  base64_encode(file_path) {
+    // read binary data
+    var file = fs.readFileSync(file_path).toString();
+    console.log(file);
+    
+    // convert binary data to base64 encoded string
+    let b64 = new Buffer.from(file).toString('base64');
+    console.log(b64);
+    return b64;
+  }
+
+  getKeys(region) {
+    AWS.config.update({region: region});
+    let ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+    ec2.describeKeyPairs({}, (err, data) => {
+      if(err) {
+        console.error(err);
+      }
+      else {
+        console.log(data);
+      }
+    });
+  }  
+
   createInstance(instanceType, name, region, keyName, instanceCount, userDataPath) {
+    AWS.config.update({region: region});
+
     var instanceParams = {
       ImageId: this.amis[region], 
       InstanceType: instanceType,
@@ -24,6 +58,7 @@ class EC2Launcher {
       MaxCount: instanceCount,
       UserData: this.base64_encode(userDataPath)
    };
+   console.log(instanceParams);
 
     // Create a promise on an EC2 service object
     var instancePromise = new AWS.EC2({apiVersion: '2016-11-15'}).runInstances(instanceParams).promise();
@@ -35,44 +70,28 @@ class EC2Launcher {
       var instanceId = data.Instances[0].InstanceId;
       console.log("Created instance", instanceId);
       // Add tags to the instance
-      tagParams = {Resources: [instanceId], Tags: [
-          {
-            Key: 'Name',
-            Value: 'SDK Sample'
-          }
-      ]};
+      // tagParams = {Resources: [instanceId], Tags: [
+      //     {
+      //       Key: 'Name',
+      //       Value: 'SDK Sample'
+      //     }
+      // ]};
       // Create a promise on an EC2 service object
-      var tagPromise = new AWS.EC2({apiVersion: '2016-11-15'}).createTags(tagParams).promise();
+      // var tagPromise = new AWS.EC2({apiVersion: '2016-11-15'}).createTags(tagParams).promise();
       // Handle promise's fulfilled/rejected states
-      tagPromise.then(
-        function(data) {
-          console.log("Instance tagged");
-        }).catch(
-          function(err) {
-          console.error(err, err.stack);
-        });
+      // tagPromise.then(
+      //   function(data) {
+      //     console.log("Instance tagged");
+      //   }).catch(
+      //     function(err) {
+      //     console.error(err, err.stack);
+      //   });
     }).catch(
       function(err) {
       console.error(err, err.stack);
     });
-
-
   }
 }
 
-// Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
-// Load credentials and set region from JSON file
-AWS.config.update({region: 'REGION'});
-
-// Create EC2 service object
-var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
-
-// AMI is amzn-ami-2011.09.1.x86_64-ebs
-var instanceParams = {
-   ImageId: 'AMI_ID', 
-   InstanceType: 't2.micro',
-   KeyName: 'KEY_PAIR_NAME',
-   MinCount: 1,
-   MaxCount: 1
-};
+launcher = new EC2Launcher();
+launcher.go();
