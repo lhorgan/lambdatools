a6108481d61f58363e6ff16236110851f183908f
Luke Horgan <horgan.l@husky.neu.edu>
2020-08-26 00:21:33 -0400

    listFunctionsHelper works

--- lambda_launcher.js
+++ lambda_launcher.js
@@ -1,4 +1,4 @@
-//const exec = util.promisify(require('child_process').exec);
+const AWS = require('aws-sdk');
 
 const { exec } = require("child_process");
 const { stderr } = require("process");
@@ -8,8 +8,8 @@ const fs = require('fs');
 // clever pattern from https://dev.to/sobiodarlington/better-error-handling-with-async-await-2e5m
 const handle = (promise) => {
   return promise
-    .then(data => ([data, undefined]))
-    .catch(error => Promise.resolve([undefined, error]));
+    .then(data => ([data, null]))
+    .catch(error => Promise.resolve([null, error]));
 }
 
 class LambdaLauncher {
@@ -17,13 +17,16 @@ class LambdaLauncher {
   }
 
   async go() {
-    let [result, error] = await handle(this.zipDirectory("./testzip", "testzip.zip"));
-    if(error) {
-      console.error(error);
-    }
-    else {
-      console.log("Success!");
-    }
+    // let [result, error] = await handle(this.zipDirectory("./testzip", "testzip.zip"));
+    // if(error) {
+    //   console.error(error);
+    // }
+    // else {
+    //   console.log("Success!");
+    // }
+
+    let res = await this.listFunctionsHelper("us-east-1", {});
+    console.log(res);
   }
 
   createBucket(bucketParams) {
@@ -54,6 +57,91 @@ class LambdaLauncher {
         }
         else {
           resolve(data);
+        }
+      });
+    });
+  }
+
+  launch(region, S3BucketName, S3KeyName, config) {
+    console.log("Attempting to launch " + name);
+    AWS.config.update({region: region});
+    let lambda = new AWS.Lambda();
+
+    var params = {
+      Code: {
+          //ZipFile: fs.readFileSync("lambda.zip")
+          S3Bucket: S3BucketName,
+          S3Key: S3KeyName
+      }
+    };
+    for(let fieldName in config) {
+      params[fieldName] = config[fieldName];
+    }
+
+    lambda.createFunction(params, (err, data) => {
+        if(err) {
+            console.log(err);
+            console.log("Failed to launch " + name);
+        }
+        else {
+            console.log("Launched " + name);
+        }
+    });
+  }
+
+  async iterateFunction(f, milliseconds, count) {
+    return new Promise((accept, reject) => {
+      let i = 0;    
+      let interval = setInterval(async () => {
+        let [data, error] = await handle(f());
+        i++;
+        if(i === count) {
+          clearInterval(interval);
+          accept(data)
+        }
+
+        if(error) {
+          clearInterval(interval);
+          reject(error);
+        }
+      }, milliseconds);
+    });
+  }
+
+  async listFunctions(namespace, region) {
+    let nextMarker = null;
+
+    do {
+      let [functionData, error] = await handle(this.listFunctionsHelper(region, params));
+      if(error) { // reject the promise
+        throw new Error(error);
+      }
+      nextMarker = functionData.nextToken;
+    } while(nextMarker !== null);
+
+    return true;
+  }
+
+  async listFunctionsHelper(region, params) {
+    AWS.config.update({region: region});
+    let lambda = new AWS.Lambda();
+
+    return new Promise((accept, reject) => {
+      lambda.listFunctions(params, (err, data) => {
+        if(err) {
+          console.error(err, err.stack);
+          reject(err);
+        }
+        else {
+          let lambdas = data.Functions;
+          let lambdaNames = [];
+
+          for(let i = 0; i < lambdas.length; i++) {
+            lambdaNames.push(lambdas[i].FunctionName);
+          }
+
+          let nextMarker = data.NextMarker;
+          accept({lambdaNames, nextMarker});
         }
       });
     });
