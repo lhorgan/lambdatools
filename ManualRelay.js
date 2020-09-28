8f20a29f5a2d10cc015e5bc5c59efc338c693c52
lhorgan <soystuff@gmail.com>
2020-09-28 05:54:41 +0000

    grk

--- ManualRelay.js
+++ ManualRelay.js
@@ -1,12 +1,37 @@
+const AWS = require('aws-sdk');
+
 const Relay = require('./relay.js').Relay;
 
 class ManualRelay extends Relay {
   constructor() {
     super("8081");
+
+    this.invokeLambdas([{"name": "TestFunc120", "region": "us-east-1"}]);
   }
 
-  invokeLambda() {
+  invokeLambda(lambdaInfo) {
     console.log("We are invoking a Lambda!");
+
+    AWS.config.update({region: lambdaInfo.region});
+    var lambda = new AWS.Lambda();
+    let payload = {"relayURLs": ["http://172.31.74.199:8081"]};
+
+    let params = {
+      FunctionName: lambdaInfo.name,
+      Payload: JSON.stringify(payload)
+    };
+
+    console.log("INVOKING WITH PARAMS ");
+    console.log(params);
+
+    lambda.invoke(params, (err, data) => {
+      if(err) {
+        console.error(err);
+      }
+      else {
+        console.log(data);
+      }
+    });
   }
 }
 
