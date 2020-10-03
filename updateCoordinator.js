a08fe2e02fc5d825fc19116ea5a37c10a5b16804
Luke Horgan <horgan.l@husky.neu.edu>
2020-10-03 22:34:48 +0000

    welp, it doesn't immediately crash

--- updateCoordinator.js
+++ updateCoordinator.js
@@ -8,9 +8,15 @@ class Updater {
 
     this.server = require('http').createServer(this.app);
     this.io =  require('socket.io')(this.server);
+
+    this.io =  require('socket.io')(this.server);
+    this.coordinatorNamespace = this.io.of("/coordinator");
+    this.lambdaNamespace = this.io.of("/lambda");
     
     this.relayNamespace = config.relayNamespace;
     this.relayPort = config.relayPort;
+    this.relayIO = this.io.of("/relay");
+
     this.port = config.port;
     
     this.server.listen(this.port, ()  => {
@@ -18,6 +24,17 @@ class Updater {
     });
 
     this.listenSocket();
+  }
+
+  listenSocket() {
+    this.relayIO.on("connect", (socket) => {
+      console.log("Coordinator connected");
+      if(this.coordinatorSocket) {
+        console.log("Appears the coordinator reconnected...");
+        this.coordinatorSocket.disconnect();
+      }
+      this.coordinatorSocket = socket;
+    });
   }
 
   async getRelayURLs() {
@@ -50,17 +67,21 @@ class Updater {
   }
 
   connectToRelays() {
-    let relayURLs = this.getRelayURLs();
+    /*let relayURLs = this.getRelayURLs();
 
     for(let i = 0; i < instances.Reservations.length; i++) {
       for(let j = 0; j < instances.Reservations[i].Instances.length; j++) {
         let instance = instances.Reservations[i].Instances[j];
         this.addRelaySocket(`http://${instance.PrivateIpAddress}:${this.relayPort}`);
       }
-    }
+    }*/
   }
 
-  listenSocket() {
+  addRelaySocket(socket) {
+    
+  }
+
+  /*listenSocket() {
     this.relayNamespace.on("connect", (socket) => {
       console.log("Someone joined!");
       let address = socket.handshake.address;
@@ -69,7 +90,11 @@ class Updater {
         console.log(message);
       });
     });
-  }
+  }*/
 }
 
-let updater = new Updater();
+let updater = new Updater({
+  relayNamespace: "whylord",
+  relayPort: "4101",
+  port: "5101"
+});
