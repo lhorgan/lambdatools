87b37f178d8139648054b0e2efc404d86da3f636
lhorgan <soystuff@gmail.com>
2020-10-04 21:05:58 +0000

    haha, the basic http function works!

--- updateRelay.js
+++ updateRelay.js
@@ -1,34 +1,40 @@
 class Updater {
-  constructor(port, coordPort) {
+  constructor(port) {
     let express = require("express");
 
     this.io = require('socket.io-client');
     this.app = express();
     this.server = require('http').createServer(this.app);
+    const bodyParser = require("body-parser");
 
     this.port = port;
-    this.coordPort = coordPort;
+    this.coordURL = null;
 
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
     this.listenHTTP();
-
-    // this.socket = this.io(`${coordURL}/relay`, {query: {}});
-    // this.socket.on("message", (message) => {
-    //   if(message.type === "update") {
-    //     this.executeScript(message.script);
-    //   }
-    // });
   }
 
   listenHTTP() {
     // Listen on the port specified in console args
-    this.server.listen(this.port, ()  => {
+    this.app.listen(this.port, ()  => {
       console.log("App is listening on port " + this.port);
     });
     
-    this.app.get("/relayMan", (req, res) => {
-      //this.coordAddress = req.body;
-      console.log("Coordinator connected!");
+    this.app.post("/relay", (req, res) => {
+      console.log("Coordinator connected!!!");
+      console.log(req.body.coordURL);
+      this.listenSocket();
       res.send({"status": 200});
+    });
+  }
+
+  listenSocket() {
+    this.socket = this.io(`${this.coordURL}/relay`, {query: {}});
+    this.socket.on("message", (message) => {
+      if(message.type === "update") {
+        this.executeScript(message.script);
+      }
     });
   }
   
@@ -48,4 +54,4 @@ class Updater {
   }
 }
 
-let updater = new Updater("8000", "5101");
+let updater = new Updater("8000");
