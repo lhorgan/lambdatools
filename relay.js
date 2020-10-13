01ffb8aae8faa00ab12c283bb8e6cec69976162e
lhorgan <soystuff@gmail.com>
2020-10-13 07:50:10 +0000

    smaller depth

--- relay.js
+++ relay.js
@@ -29,7 +29,7 @@ class Relay {
 
     this.lambdaInfos = {};
 
-    this.maxDepth = 500; // max number of lambdas per function name
+    this.maxDepth = 5; // max number of lambdas per function name
 
     this.completedJobs = [];
     this.scale();
